59602f8eea7013ca145844514af5a3869cff661c
3arfa11 <arafa4881@gmail.com>
2023-12-23 22:45:35 +0200

    fucj

--- src/main/java/org/example/demo1/HelloApplication.java
+++ src/main/java/org/example/demo1/HelloApplication.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        System.out.println("ARAFA`");
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
