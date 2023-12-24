345f6f9addf9c590c569320668209ed29084f75a
Mhmd0Mhmod <iammhmdmhmod@gmail.com>
2023-12-24 23:55:56 +0200

    new Update

--- src/main/java/com/example/demo10/DashboardController.java
+++ src/main/java/com/example/demo10/DashboardController.java
@@ -1,55 +1,13 @@
 package com.example.demo10;
 
 import DataBase_Classes.User;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
-
-import java.io.IOException;
 
 public class DashboardController {
     private User currentUser;
-    @FXML
-    private Text welcomeText;
-    @FXML
-    private Pane MainPane;
 
-    @FXML
-    private Pane layerOne;
-    @FXML
-    private Pane layerTwo;
-    @FXML
-    private Pane layerThree;
-    @FXML
-    private Pane layerFour;
-    @FXML
-    private Button logOut;
-    public void setCurrentUser(User currentUser) {
+    public DashboardController(User currentUser) {
         this.currentUser = currentUser;
     }
 
-    public void setWelcomeText() {
-        welcomeText.setText("Welcome " + currentUser.getUsername());
-    }
-    public void dashboard(ActionEvent event){
-        layerOne.toFront();
-    }
-    public void transation(ActionEvent event){
-        layerTwo.toFront();
-    }
-    public void changeCurrency(ActionEvent e){
-        layerThree.toFront();
-    }
-    public  void accountData(ActionEvent e){
-        layerFour.toFront();
-    }
-    public void doLogout(ActionEvent e) throws IOException {
-        new LoadScene("login.fxml",((Node) e.getSource()).getScene()).createScene();
-    }
 
 }
--- src/main/java/com/example/demo10/LoadScene.java
+++ src/main/java/com/example/demo10/LoadScene.java
@@ -10,15 +10,15 @@ import java.io.IOException;
 
 public class LoadScene {
     private String resource;
-    private ActionEvent event;
-    LoadScene(String resource, ActionEvent event){
+    private Scene scene;
+    LoadScene(String resource, Scene scene){
         this.resource=resource;
-        this.event=event;
+        this.scene=scene;
     }
     public void createScene() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(resource));
         Scene signUpScene = new Scene(fxmlLoader.load());
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) scene.getWindow();
         // Set and show the secondary scene on the current stage
         stage.setScene(signUpScene);
         stage.show();
