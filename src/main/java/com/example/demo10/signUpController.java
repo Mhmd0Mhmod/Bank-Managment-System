3d222a6d2a2d5f7dc8574aced8f98e6ff2d3f288
Mohamed Shehab <mohamedshepo280@gmail.com>
2023-12-24 02:55:39 +0200

    InserUser

--- src/main/java/com/example/demo10/signUpController.java
+++ src/main/java/com/example/demo10/signUpController.java
@@ -1,9 +1,11 @@
 package com.example.demo10;
 
+import DataBase_Classes.InsertUser;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 
 
 
@@ -12,8 +14,27 @@ public class signUpController {
     @FXML
     private ComboBox<String> currencyComboBox;
     @FXML
+    private TextField emailTextField;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordTextField;
+    @FXML
+    private RadioButton maleRadioButton;
+    @FXML
+    private RadioButton femaleRadioButton;
+    private ToggleGroup gender;
+    @FXML
+    private RadioButton privaryRadioButton;
+    @FXML
+    private Button registerButton;
     public void initialize(){
         currencyComboBox.setItems(currencies);
-
     }
+    public void registerButtonOnAction(ActionEvent event){
+//        InsertUser insertUser = new InsertUser(usernameTextField.getText(),emailTextField.getText(),passwordTextField.getText()
+//        ,currencyComboBox.getValue(),);
+        System.out.println(gender.getSelectedToggle());
+    }
+
 }
