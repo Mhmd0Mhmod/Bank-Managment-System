ebe62cb3401ed03551f76c5d41305a40c7af2adf
Mohamed Shehab <mohamedshepo280@gmail.com>
2023-12-26 19:49:01 +0200

    userdashboard updated

--- src/main/java/com/example/demo10/userDashboardController.java
+++ src/main/java/com/example/demo10/userDashboardController.java
@@ -1,15 +1,20 @@
 package com.example.demo10;
 
+import DataBase_Classes.DataBaseConnection;
+import DataBase_Classes.Loan;
 import DataBase_Classes.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class userDashboardController {
     private User currentUser;
@@ -17,7 +22,8 @@ public class userDashboardController {
     private Text welcomeText;
     @FXML
     private Pane MainPane;
-
+    private DataBaseConnection Connection = new DataBaseConnection();
+    private java.sql.Connection connectionDB = Connection.getConnection();
     @FXML
     private Pane layerOne;
     @FXML
@@ -30,6 +36,20 @@ public class userDashboardController {
     private Button logOut;
     @FXML
     private Button loanButton;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private PasswordField passwordPasswordField;
+    @FXML
+    private ComboBox<String> currencyDropList;
+    @FXML
+    private Label deleteAccountLabel;
+    @FXML
+    private Label gmailLabel;
+    @FXML
+    private Label githubLabel;
     public void loanButtonOnAction(ActionEvent event) throws SQLException {
         Loan loan=new Loan(currentUser);
 //        loan.requestLoan(5000,"Personal");
@@ -53,6 +73,7 @@ public class userDashboardController {
     }
     public  void accountData(ActionEvent e){
         layerFour.toFront();
+        setUserData();
     }
     public void doLogout(ActionEvent e) throws IOException {
         new LoadScene("login.fxml",((Node) e.getSource()).getScene()).createScene();
@@ -60,5 +81,53 @@ public class userDashboardController {
     public void addOneOnAction(ActionEvent event) throws IOException {
         transation(event);
     }
+    public void setUserData(){
+        this.usernameTextField.setText(currentUser.getUsername());
+        this.emailTextField.setText(currentUser.getEmail());
+        this.passwordPasswordField.setText(currentUser.getPassword());
+        this.currencyDropList.setValue(currentUser.getCurrency());
+        this.githubLabel.setText(currentUser.getUsername());
+        this.gmailLabel.setText(currentUser.getEmail());
+    }
+    public void deleteAccount(ActionEvent event) throws SQLException, IOException {
+
+    String checkLoan="SELECT COUNT(*) AS COUNT FROM loans WHERE user_id='"+currentUser.getId()+"';";
+    String delete="delete from users where id='"+currentUser.getId()+"';";
+
+    Statement statement = connectionDB.createStatement();
+    ResultSet result = statement.executeQuery(checkLoan);
+    if (result.next()){
+        int numOfLoans=result.getInt("COUNT");
+        if (numOfLoans>0) deleteAccountLabel.setText("You can't delete your account you still need to pay  " + numOfLoans + " loans");
+        else {
+
+            Alert confirmAlert = new Alert(Alert.AlertType.CONFIRMATION);
+            confirmAlert.setTitle("Confirmation Dialog");
+            confirmAlert.setHeaderText("Confirmation Needed");
+            confirmAlert.setContentText("Are you sure to delete your account?");
+            confirmAlert.showAndWait().ifPresent(response -> {
+                if (response == ButtonType.OK) {
+                    Statement statement2 = null;
+                    try {
+                        statement2 = connectionDB.createStatement();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    try {
+                        statement2.executeUpdate(delete);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+
+            new LoadScene("login.fxml", ((Node) event.getSource()).getScene()).createScene();
+
+        }
+        }
+    }
+
 
 }
+
+
