51b26c01c53ac147a347b0c87d30a58115a4b30b
3arfa11 <arafa4881@gmail.com>
2023-12-26 19:06:24 +0200

    SplashScreen

--- src/main/java/com/example/demo10/HelloApplication.java
+++ src/main/java/com/example/demo10/HelloApplication.java
@@ -13,7 +13,7 @@ import javafx.stage.StageStyle;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("splashScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 //        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
