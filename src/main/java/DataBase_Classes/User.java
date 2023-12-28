8951eecf2ee33bb64bba7e72ae310c37c2631caf
yazed2003 <yazedelkholy2003@gmail.om>
2023-12-28 02:02:28 +0200

    final touches

--- src/main/java/DataBase_Classes/User.java
+++ src/main/java/DataBase_Classes/User.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 
 public class User {
     private String username;
-    //    private User currentUser;
+
     private String password;
     private String email;
     private String currency;
